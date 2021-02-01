734b0b1847d6b43fe96b12dbf3ca8e82607fe68c
SalouaK <saloua.ktb@gmail.com>
2021-02-01 18:25:45 +0100

    Challenge Javascript

--- js/exos.js
+++ js/exos.js
@@ -7,9 +7,30 @@
 
 /* Exo 1 */
 // Ton code ici...
-
+var score = 0;
+var question1 = 'Quelle mer borde la ville de Sébastopol ?';
+var solution1 = 'La mer Noire';
+var reponse1 = prompt(question1);
+if (reponse1 === solution1){
+    alert('Gagné !')
+    score = score + 1
+}
+else {
+    alert('Perdu...')
+}
 /* Exo 2 */
 // Ton code ici...
-
+var question2 = 'Quel est l\'âge du capitaine ?';
+var solution2 = '63';
+var reponse2 = prompt(question2);
+if (reponse2 === solution2){
+    alert('Gagné !')
+    score = score +1
+}
+else {
+    alert('Perdu...')
+}
+parseInt (63)
 /* Exo 3 */
 // Ton code ici...
+alert('Ton score est de : ' + score + '/2')
